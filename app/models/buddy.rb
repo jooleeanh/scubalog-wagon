5bf232abca0fcb6a95ce0dd9f1252d1c4fc6b3fb
Julian Honma <julian.honma@gmail.com>
2016-11-21 17:11:26 +0100

    Add buddies and animal models. ANIMAL has no associations.

---
+++ app/models/buddy.rb
@@ -0,0 +1,4 @@
+class Buddy < ApplicationRecord
+  belongs_to :user
+  belongs_to :dive
+end
---
+++ db/migrate/20161121152917_create_buddies.rb
@@ -0,0 +1,10 @@
+class CreateBuddies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :buddies do |t|
+      t.references :user, foreign_key: true
+      t.references :dive, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

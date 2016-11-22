78e9ffacb04881901fb1272ffe6d06585f5cbaa1
Julian Honma <julian.honma@gmail.com>
2016-11-22 13:44:26 +0100

    create buddies controller

---
+++ app/controllers/buddies_controller.rb
@@ -0,0 +1,32 @@
+class BuddiesController < ApplicationController
+  before_action :find_dive, only: [:create]
+  before_action :find_buddy, only: [:update, :delete]
+
+  def create
+    @buddy = @dive.buddies.new(buddy_params)
+    @buddy.save
+    # TODO: if, redirect, render?
+  end
+
+  def update
+    @buddy.update(buddy_params) # user_id and dive_id already permitted?
+  end
+
+  def delete
+    @buddy.destroy
+  end
+
+  private
+
+  def find_buddy
+    @buddy = Buddy.find(params[:id])
+  end
+
+  def find_dive
+    @dive = Dive.find(params[:dive_id])
+  end
+
+  def buddy_params
+    params.require(:buddy).permit(:user_id)
+  end
+end

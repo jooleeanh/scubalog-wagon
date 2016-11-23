f3ce95f5f132867f9d96972efe3cdf151e8ecdcd
Theophile Sourdille <theophile.sourdille@yahoo.fr>
2016-11-23 18:24:00 +0100

    Gmaps fixed and events#show prepared

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -19,8 +19,7 @@ Geocoder.configure(
   # units: :mi,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
   :units => :km,
-
   :lookup    => :google,
-  :api_key   => ENV['GOOGLE_API_SERVER_KEY'],
+  :api_key   => ENV['GOOGLE_API_KEY'],
   :use_https => true,
 )

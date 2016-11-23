c9f6f5237702a1b416a7f59f8113207f13c42486
Julian Honma <julian.honma@gmail.com>
2016-11-23 11:14:41 +0100

    Fix routes.rb to have proper nesting (put user-dependent models at root)

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:show, :destroy], shallow: true do
-    resources :equipment_sets, only: [:new, :create, :edit, :update, :destroy]
-    resources :participations, only: [:create, :update, :destroy]
+  resources :users, only: [:show, :destroy]
+  resources :equipment_sets, only: [:new, :create, :edit, :update, :destroy]
+  resources :participations, only: [:create, :update, :destroy]
+
+  resources :divesites do
+    resources :events
   end
+
   resources :dives do
     resources :buddies
     resources :sightings, only: [:create, :update, :destroy]
     resources :data_points, only: [:create]
-  end
-  resources :divesites do
-    resources :events
   end
 end

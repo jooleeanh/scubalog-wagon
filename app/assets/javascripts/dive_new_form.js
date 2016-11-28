96b8caedf4056fe0f319e1e8e1adf4e35c5bbd66
Julian Honma <julian.honma@gmail.com>
2016-11-28 23:39:36 +0100

    make the inputs auto-zoom on tab

--- app/assets/javascripts/dive_new_form.js
+++ app/assets/javascripts/dive_new_form.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.dive-new-form-field input,select,textarea').focusin(function() {
     $(this).parents('.dive-new-form-field').toggleClass('log-active');
+    $(this).parents('.dive-new-form-field').trigger("click");
     // TODO:  Simulate click on next input to zoom to next.
   });
 

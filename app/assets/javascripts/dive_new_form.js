6ad9c868c7a9d6b08a0ae97a03e57254270aca8d
Julian Honma <julian.honma@gmail.com>
2016-11-30 01:26:50 +0100

    add icons to form

--- app/assets/javascripts/dive_new_form.js
+++ app/assets/javascripts/dive_new_form.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.dive-new-form-field input,select,textarea').focusin(function() {
-    $(this).parents('.dive-new-form-field').toggleClass('log-active');
+    $(this).parents('.dive-input').toggleClass('log-active');
     $(this).parents('.dive-new-form-field').trigger("click");
     // TODO:  Simulate click on next input to zoom to next.
   });
 
   $('.dive-new-form-field input,select,textarea').focusout(function() {
-    $(this).parents('.dive-new-form-field').toggleClass('log-active');
+    $(this).parents('.dive-input').toggleClass('log-active');
   })
 });

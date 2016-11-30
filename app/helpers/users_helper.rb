0321f9b8e2e908fb62500f9dd8300c1c076856be
Julian Honma <julian.honma@gmail.com>
2016-11-30 17:33:39 +0100

    add real stats in first boxes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,26 @@ module UsersHelper
       return image_tag("user_avatar.png", class: "user-show-photo")
     end
   end
+
+  def compute_total_divetime(user)
+    total = 0
+    user.dives.each { |dive| total += dive.bottom_time }
+    total
+  end
+
+  def compute_longest_dive(user)
+    user.dives.order(bottom_time: :DESC).first.bottom_time
+  end
+
+  def compute_deepest_dive(user)
+    user.dives.order(max_depth: :DESC).first.max_depth.round
+  end
+
+  def compute_sightings(user)
+    sightings = 0
+    user.dives.each do |dive|
+      sightings += dive.sightings.count
+    end
+    sightings
+  end
 end
